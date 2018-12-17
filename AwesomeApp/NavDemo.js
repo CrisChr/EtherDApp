03363a8345a7d4f81a7071dc7417da6952057b1f
Hong ru Cai <cdcaihr@cn.ibm.com>
2018-12-17 18:38:05 +0800

    add tab icons and Truffle package

--- AwesomeApp/NavDemo.js
+++ AwesomeApp/NavDemo.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {StyleSheet, Text, View, Button} from 'react-native';
+import {StyleSheet, Text, View, Button, Image} from 'react-native';
 import { createAppContainer, createBottomTabNavigator} from 'react-navigation';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import HomeContainer from './FirstPage';
 import Friends from './Friends';
@@ -32,19 +33,31 @@ class AccountScreen extends React.Component {
 
 const stackNav = createBottomTabNavigator(
   {
-    Home: HomeContainerScreen,
+    Home: {
+      screen: HomeContainerScreen,
+      tabBarIcon: ({focused, horizontal, tintColor}) => (
+          focused ? <Image source={require('./img/home_active.png')} style={{ height: 30, width: 30, tintColor: 'white'}}/>
+            : <Image source={require('./img/home_inactive.png')} style={{ height: 30, width: 30, tintColor: 'white'}}/>
+      )
+    },
     Friends: FriendsScreen,
     Account: AccountScreen,
   },
   {
     initialRouteName: 'Home', //Initial screen
     tabBarOptions: {
-      activeTintColor: '#000000',
-      activeBackgroundColor: '#d6ecf0'
+      showIcon: true
     }
   }
 );
 
 const Nav = createAppContainer(stackNav);
+
+/*const styles = StyleSheet.create({
+  icon: {
+    height: 26,
+    width: 26
+  }
+});*/
 
 export default Nav;
