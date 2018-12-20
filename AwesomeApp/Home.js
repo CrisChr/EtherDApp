946d262358f28113f7ab84b371eeba7d35e8aae2
Hong ru Cai <cdcaihr@cn.ibm.com>
2018-12-20 15:34:40 +0800

    can generate different address by different mnemonic word

--- AwesomeApp/Home.js
+++ AwesomeApp/Home.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import {StyleSheet, Text, View, Alert, Button} from 'react-native';
+import {StyleSheet, Text, View, Alert, Button, ScrollView} from 'react-native';
 import {Card} from 'react-native-elements';
 import {createStackNavigator, withNavigation, createAppContainer } from 'react-navigation';
 import 'ethers/dist/shims.js';
 import { ethers } from 'ethers';
 
 export default class WalletContainer extends React.Component {
-
   constructor(props){
     super(props);
     this.state = {
       mnemonic: '',
-      walletAddress: ''
+      address: '',
+      initialAccount: 0
     }
   }
+
+  addressList = []
+  key = 0
 
   async componentWillMount() {
     let newMnemonic = ethers.utils.HDNode.entropyToMnemonic(ethers.utils.randomBytes(16));
@@ -22,29 +25,65 @@ export default class WalletContainer extends React.Component {
     })
   }
 
-  createWallet() {
-    let wallet = ethers.Wallet.fromMnemonic(this.state.mnemonic, 'm/44\'/60\'/0\'/0/0', 0);
+  createAccount() {
+    let wallet = ethers.Wallet.fromMnemonic(this.state.mnemonic, 'm/44\'/60\'/0\'/0'+this.state.initialAccount);
+    this.key = this.state.initialAccount
+    this.addressList.push(wallet.address)
     this.setState({
-      walletAddress: wallet
+      address: this.addressList,
+      initialAccount: this.key + 1
+    })
+  }
+
+  refreshMnemonic() {
+    let newMnemonic = ethers.utils.HDNode.entropyToMnemonic(ethers.utils.randomBytes(16));
+    this.setState({
+      mnemonic: newMnemonic
     })
   }
 
   render(){
     return(
-      <View style={{ padding: 20}}>
+      <View style={{ margin: 20, flex: 1}}>
         <Card title="Mnemonic Words">
-          <Text style={{ fontWeight: 'bold', color: 'red', textAlign: 'center'}}>
+          <Text style={styles.mnemonic}>
             {this.state.mnemonic}
           </Text>
-<Button onPress={() => this.createWallet()} title="Create Account"/>
         </Card>
-
-        <Text>{this.state.walletAddress}</Text>
+        <View style={{marginTop:20}}></View>
+        <Button onPress={() => this.refreshMnemonic()} title="Refresh Mnemonic Word"/>
+        <View style={{marginTop:20}}></View>
+        <Button onPress={() => this.createAccount()} title="Create Account" style={styles.button}/>
+        <ScrollView>
+          <Items items={this.state.address} />
+        </ScrollView>
       </View>
     )
   }
 }
 
-//const HomeView = withNavigation(HomeScreen);
+class Items extends React.Component {
+  render(){
+    let addressList = []
+    for(var i=0; i<this.props.items.length; i++){
+      addressList.push(
+       <Text key={i} style={styles.accountStyle}>Address {i+1}: {this.props.items[i]}</Text>
+      )
+    }
+    return (
+      <View>{addressList}</View>
+    )
+  }
+}
 
-//export default HomeView;
+const styles = StyleSheet.create({
+ mnemonic: {
+   fontWeight: 'bold',
+   color: 'red',
+   textAlign: 'center'
+ },
+ accountStyle: {
+   margin: 10,
+   fontWeight: 'bold'
+ }
+})
