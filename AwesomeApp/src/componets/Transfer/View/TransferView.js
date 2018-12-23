2de71a2c01fc20e27897171bc74c306e45a20edf
Crischr <chr7749@hotmail.com>
2018-12-23 14:02:32 +0800

    Add project

---
+++ AwesomeApp/src/componets/Transfer/View/TransferView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export default class Transfer extends React.Component {
+ render(){
+   return(
+     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+       <Text>This is Friends!</Text>
+     </View>
+   )
+ }
+}
